4774b4fe3b15c5be99934589ac97de66f743245e
NoName <57182847+ali-now447@users.noreply.github.com>
2024-01-26 20:49:39 +0300

    Update download.py

--- ZelzalMusic/plugins/play/download.py
+++ ZelzalMusic/plugins/play/download.py
@@ -23,7 +23,7 @@ def remove_if_exists(path):
         os.remove(path)
 
 
-@app.on_message(command(["/song", "بحث", "/music"]))
+@app.on_message(command(["/song", "بحث", "يوت"]))
 async def song_downloader(client, message: Message):
     query = " ".join(message.command[1:])
     m = await message.reply_text("<b>⇜ جـارِ البحث عـن المقطـع الصـوتـي . . .</b>")
