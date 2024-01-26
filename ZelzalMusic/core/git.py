cc2a9ea923d5a63bc56e39862959e7c0a1e7a5b7
NoName <57182847+ali-now447@users.noreply.github.com>
2024-01-27 00:02:43 +0300

    Update git.py

--- ZelzalMusic/core/git.py
+++ ZelzalMusic/core/git.py
@@ -46,9 +46,10 @@ def git():
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
     try:
-        LOGGER("ميــوزك زدثــون").info(f"Git Client Found [VPS DEPLOYER]")
+        repo = Repo()
+        LOGGER("ميــوزك الـجـزار").info(f"Git Client Found [VPS DEPLOYER]")
     except GitCommandError:
-        LOGGER("ميــوزك زدثــون").info(f"Invalid Git Command")
+        LOGGER("ميــوزك الـجـزار").info(f"Invalid Git Command")
     except InvalidGitRepositoryError:
         repo = Repo.init()
         if "origin" in repo.remotes:
@@ -75,4 +76,4 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGGER("ميــوزك زدثــون").info(f"جارِ الكشف عن تحديثات جديدة . . .")
+        LOGGER("ميــوزك الـجـزار").info(f"جارِ الكشف عن تحديثات جديدة . . .")
