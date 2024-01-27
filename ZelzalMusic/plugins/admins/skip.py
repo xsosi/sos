fcf1144601d6fd7c63e23c74c011b35110db90a9
NoName <57182847+ali-now447@users.noreply.github.com>
2024-01-27 20:27:32 +0300

    Update skip.py

--- ZelzalMusic/plugins/admins/skip.py
+++ ZelzalMusic/plugins/admins/skip.py
@@ -19,7 +19,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    command(["/skip", "تخطي", "/next", "التالي"]) & ~BANNED_USERS
+    command(["/skip", "تخطي", "/next", "سكب"]) & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
