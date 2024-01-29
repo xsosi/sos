d587f12637f214d7b9b99138b3ba366a41b540f0
Hjwad <158073869+Hjwad@users.noreply.github.com>
2024-01-29 19:10:27 +0300

    Update must_join.py

--- ZelzalMusic/plugins/tools/must_join.py
+++ ZelzalMusic/plugins/tools/must_join.py
@@ -1,1 +1,31 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from ZelzalMusic import app
 
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not "https://t.me/mmmsc":  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member("mmmsc", msg.from_user.id)
+        except UserNotParticipant:
+            if "https://t.me/mmmsc".isalpha():
+                link = "https://t.me/mmmsc"
+            else:
+                chat_info = await bot.get_chat("mmmsc")
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"⌯︙عذࢪاَ حَبيبي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة Not ᥉ꪮ᥉ .\n⌯︙قناة : @mmmsc .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("Not ᥉ꪮ᥉", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I'm not admin in the MUST_JOIN chat @mmmsc !")
