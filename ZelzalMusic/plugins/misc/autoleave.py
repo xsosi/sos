dc0ffec44c5fa1b445e1e639de0242a9d549f9c2
Hjwad <158073869+Hjwad@users.noreply.github.com>
2024-02-02 01:17:42 +0300

    Update autoleave.py

--- ZelzalMusic/plugins/misc/autoleave.py
+++ ZelzalMusic/plugins/misc/autoleave.py
@@ -15,7 +15,7 @@ from ZelzalMusic.utils.database import get_client, is_active_chat, is_autoend
 
 async def auto_leave():
     if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(60000):
+        while not await asyncio.sleep(5):
             from ZelzalMusic.core.userbot import assistants
 
             for num in assistants:
