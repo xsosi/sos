bd4bf373bf2399e9193a4ddd31d0c1f6943a3378
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-04-13 16:50:11 +0300

    Update play.py

--- ZelzalMusic/plugins/play/play.py
+++ ZelzalMusic/plugins/play/play.py
@@ -8,22 +8,22 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BOT_TOKEN
 from strings.filters import command
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import seconds_to_min, time_to_seconds
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.decorators.play import PlayWrapper
-from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
+from ZelzalMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from ZelzalMusic.core.call import Zelzaly
+from ZelzalMusic.utils import seconds_to_min, time_to_seconds
+from ZelzalMusic.utils.channelplay import get_channeplayCB
+from ZelzalMusic.utils.decorators.language import languageCB
+from ZelzalMusic.utils.decorators.play import PlayWrapper
+from ZelzalMusic.utils.formatters import formats
+from ZelzalMusic.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AnonXMusic.utils.logger import play_logs
-from AnonXMusic.utils.stream.stream import stream
+from ZelzalMusic.utils.logger import play_logs
+from ZelzalMusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 force_btn = InlineKeyboardMarkup(
@@ -306,7 +306,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await Anony.stream_call(url)
+                await Zelzaly.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
@@ -519,8 +519,8 @@ async def play_music(client, CallbackQuery, _):
     return await mystic.delete()
 
 
-@app.on_callback_query(filters.regex("AnonymousAdmin") & ~BANNED_USERS)
-async def anonymous_check(client, CallbackQuery):
+@app.on_callback_query(filters.regex("ZelzalymousAdmin") & ~BANNED_USERS)
+async def Zelzalymous_check(client, CallbackQuery):
     try:
         await CallbackQuery.answer(
             "» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ :\n\nᴏᴘᴇɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ sᴇᴛᴛɪɴɢs.\n-> ᴀᴅᴍɪɴɪsᴛʀᴀᴛᴏʀs\n-> ᴄʟɪᴄᴋ ᴏɴ ʏᴏᴜʀ ɴᴀᴍᴇ\n-> ᴜɴᴄʜᴇᴄᴋ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴs.",
@@ -530,7 +530,7 @@ async def anonymous_check(client, CallbackQuery):
         pass
 
 
-@app.on_callback_query(filters.regex("AnonyPlaylists") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("ZelzalyPlaylists") & ~BANNED_USERS)
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
