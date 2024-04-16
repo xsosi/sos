0939b27d457dd3c718812802a652a7d7a4e4935d
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-04-16 19:36:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mmqqsks:w7U5p2vlMr9mTXhI@sos.o8k1er9.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 2001))
 
