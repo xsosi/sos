a1ff16a7dae9219c4ed92530acc29958cd61d7f0
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-06-06 02:45:55 +0300

    Update call.py

--- ZelzalMusic/core/call.py
+++ ZelzalMusic/core/call.py
@@ -42,11 +42,7 @@ from ZelzalMusic.utils.stream.autoclear import auto_clean
 from ZelzalMusic.utils.thumbnails import get_thumb
 from strings import get_string
 import os, requests
-proxyDict = {
-              "http"  : os.environ.get('FIXIE_URL', ''),
-              "https" : os.environ.get('FIXIE_URL', '')
-            }
-r = requests.get('https://www.youtube.com', proxies=proxyDict)
+
 
 autoend = {}
 counter = {}
