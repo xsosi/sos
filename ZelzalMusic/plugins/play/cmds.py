662f7f1f34e94ebd2a65dac2ab57e040fa304b9e
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-06-08 05:32:44 +0300

    Update cmds.py

--- ZelzalMusic/plugins/play/cmds.py
+++ ZelzalMusic/plugins/play/cmds.py
@@ -46,32 +46,3 @@ async def zdatsr(client: Client, message: Message):
     )
 
 
-@app.on_message(command(["مطور", "المطور"]) & filters.group)
-async def zilzal(client: Client, message: Message):
-    usr = await client.get_users(OWNER_ID)
-    name = usr.first_name
-    usrnam = usr.username
-    async for photo in client.iter_profile_photos(OWNER_ID, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""ٴ<b>•────‌‌‏✯ ᴇ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
-                    
-- المطور :[{usr.first_name}](https://t.me/{OWNER})
-- اليوزر :@{usrnam} 
-- الايدي :`{usr.id}`
- 
-ٴ<b>•────‌‌‏✯ ᴇ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b> """, 
-reply_markup=InlineKeyboardMarkup(
-          [               
-            [            
-              InlineKeyboardButton (name, url=f"https://t.me/{usrnam}"),
-            ],[
-              InlineKeyboardButton("•SoS Music•", url="https://t.me/mmmsc"),
-            ],
-          ] 
-       )                 
-    )                    
-                    sender_id = message.from_user.id
-                    sender_name = message.from_user.first_name
-                    senderuser = message.from_user.username
-                    sender_user = "@{senderuser}" if senderuser else "لا يوجـد"
-                    await app.send_message(OWNER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
-                    return await app.send_message(LOGGER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
