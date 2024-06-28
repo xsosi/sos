d2673290ee0eb1b3fd8670014ee0be421f2e7221
NoName <57182847+ali-now447@users.noreply.github.com>
2024-06-28 22:22:09 +0300

    Update must_join.py

--- ZelzalMusic/plugins/tools/must_join.py
+++ ZelzalMusic/plugins/tools/must_join.py
@@ -5,20 +5,20 @@ from ZelzalMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/ye20_r":  # Not compulsory
+    if not "https://t.me/mmmsc":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("ye20_r", msg.from_user.id)
+            await bot.get_chat_member("mmmsc", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/ye20_r".isalpha():
-                link = "https://t.me/ye20_r"
+            if "https://t.me/mmmsc".isalpha():
+                link = "https://t.me/mmmsc"
             else:
-                chat_info = await bot.get_chat("ye20_r")
+                chat_info = await bot.get_chat("mmmsc")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ حَبيبي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة Not Res .\n⌯︙قناة : t.me/ye20_r 🍓.\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ حَبيبي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة Not Res .\n⌯︙قناة : t.me/mmmsc 🍓.\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("Not Res", url=link)]
@@ -28,4 +28,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @ye20_r !")
+        print(f"I'm not admin in the MUST_JOIN chat @mmmsc !")
