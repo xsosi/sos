5fa6cb8677a3a0008d50c33e748c52c93f311704
NoName <57182847+ali-now447@users.noreply.github.com>
2024-06-28 22:21:26 +0300

    Update play.py

--- ZelzalMusic/plugins/play/play.py
+++ ZelzalMusic/plugins/play/play.py
@@ -30,7 +30,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(   
-              text=f"Not Res .", url=f"t.me/ye20_r",)                        
+              text=f"Not Res .", url=f"t.me/mmmsc",)                        
         ],        
     ]
 )
@@ -38,10 +38,10 @@ async def check_is_joined(message):
     try:
         userid = message.from_user.id
         user_name = message.from_user.first_name
-        status = await app.get_chat_member("ye20_r", userid)
+        status = await app.get_chat_member("mmmsc", userid)
         return True
     except Exception:
-        await message.reply_text(f'┇عزيزي: {message.from_user.mention}\n┇أشتࢪك في قناة البوت أولاً.\n┇قناة البوت: @ye20_r 🍓. ',reply_markup=force_btn,disable_web_page_preview=False)
+        await message.reply_text(f'┇عزيزي: {message.from_user.mention}\n┇أشتࢪك في قناة البوت أولاً.\n┇قناة البوت: @mmmsc 🍓. ',reply_markup=force_btn,disable_web_page_preview=False)
         return False
 
 
